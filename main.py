5c6768443cbf84d8e598e6257bd7f829051a606c
antonburakov <anton.burakovv@gmail.com>
2024-10-02 00:53:44 +0500

    Model with users and nfts table was added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
+from app.database.models import async_main
 
 
 async def main():
+    await async_main()
     bot = Bot(token='8141265458:AAGkC-mvVD91VhDhuDTU87lfkpuAzmJeGzM')
     dp = Dispatcher()
     dp.include_router(router)
